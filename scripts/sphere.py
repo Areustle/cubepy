18a10af69badecfa13d3aabee97d88a55717e940
Alex Reustle <areustledev@gmail.com>
2023-04-26 18:32:08 -0400

    debug commit

--- scripts/sphere.py
+++ scripts/sphere.py
@@ -7,12 +7,10 @@ def integrand_sphere_v(r, _, phi):
     return np.sin(phi) * r**2
 
 
-radii = np.linspace(1, 5, int(5))
-
 value, error = cp.integrate(
     integrand_sphere_v,
     [0.0, 0.0, 0.0],
-    [radii, 2 * np.pi, np.pi],
+    [[1.0, 2.0], 2 * np.pi, np.pi],
     # rtol=1e-3,
     # atol=1e-3
     itermax=4,
